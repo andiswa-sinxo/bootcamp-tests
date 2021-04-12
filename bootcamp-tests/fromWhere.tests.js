99e9bf546337dc31944529ed8662a86b53eb1c77
Andiswa <andiswasinxo@gmail.com>
2021-04-12 16:20:31 +0200

    write bootcamp exercises

--- bootcamp-tests/fromWhere.tests.js
+++ bootcamp-tests/fromWhere.tests.js
@@ -1,6 +1,6 @@
 describe('testing the fromWhere' , function(){
     it('takes car registration no and returns the town the car is from' , function(){
-        assert.equal(fromWhere('CA'), 'Cape Town');
+        assert.equal(fromWhere('CA 111'), 'Cape Town');
     });
 
 });
