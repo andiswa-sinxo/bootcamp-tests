3de1a71ab1afd8ce6a48bc336d1570f8149098f5
Andiswa <andiswasinxo@gmail.com>
2021-04-19 15:36:51 +0200

    bellville changings

--- isFromBellville.tests.js
+++ isFromBellville.tests.js
@@ -1,8 +1,8 @@
 describe('testing the isFromBellville' , function(){
-    it('is taking a registration no and returning boolean ' , function(){
+    it('is taking a registration no and returning true ' , function(){
         assert.equal(isFromBellville('CY'), true);
     });
-    it('is taking a registration no and returning boolean ' , function(){
+    it('is taking a registration no and returning false ' , function(){
         assert.equal(isFromBellville('CA'), false);
     });
 
